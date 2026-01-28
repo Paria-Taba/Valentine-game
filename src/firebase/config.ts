a557915d3cf609dcf1b7fb78a024c604ebf3ed5e
paria <pariyataba@gmail.com>
2026-01-28 14:24:10 +0100

    Move Firebase config to environment variables

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,7 @@
 export const firebaseConfig = {
-  "projectId": "quizconnect2-45774915-d8ecb",
-  "appId": "1:975831296512:web:d99c5c046a0842e06306e5",
-  "apiKey": "AIzaSyCE9iZ6T3LDtTTDSAtJB1JTVRYb8V8MVro",
-  "authDomain": "quizconnect2-45774915-d8ecb.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "975831296512"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
 };
