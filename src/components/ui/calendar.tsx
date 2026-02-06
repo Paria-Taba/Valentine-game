fc78e07eff4ebdafb7bda377497902c1d87faac6
paria <pariyataba@gmail.com>
2026-02-06 10:08:49 +0100

    add use client in all pages

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -54,13 +54,13 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("h-4 w-4", className)} {...props} />
-        ),
-      }}
+  Chevron: ({ orientation, className }) => {
+    if (orientation === "left") {
+      return <ChevronLeft className={cn("h-4 w-4", className)} />
+    }
+    return <ChevronRight className={cn("h-4 w-4", className)} />
+  },
+}}
       {...props}
     />
   )
